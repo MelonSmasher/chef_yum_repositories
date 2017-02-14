5fb98867b8d86bb7db2064a3f1f44fc52e1adb2c
Alex Markessinis <markea@sage.edu>
2017-02-14 10:10:11 -0500

    updated descriptions

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Yum Repositories
 
+Cookbook that wraps the yum_repository resource allowing one to define repositories in attributes.
+
+---
 
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'yum_repositories'
 maintainer 'Alex Markessinis'
 maintainer_email 'markea125@gmail.com'
 license 'MIT'
-description 'Installs/Configures yum_repositories'
+description 'Cookbook that wraps the yum_repository resource allowing one to define repositories in attributes.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 %w( redhat centos fedora amazon ).each do |os|
